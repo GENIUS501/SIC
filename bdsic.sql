30df35fc60c680468813d37ad19d7b0e14ae6767
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2020-03-01 10:58:13 -0600

    1

---
+++ bdsic.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE SIC
+GO 
+
+USE SIC
+GO
+
+CREATE TABLE Tab_Perfiles
+(
+	Id_Perfil	Int PRIMARY KEY,
+	Nombre_Perfil	VARCHAR(30)
+);
+
+CREATE TABLE Tab_Usuarios
+(
+	Cedula	INT UNIQUE,
+	Nombre_Usuario	VARCHAR(25) PRIMARY KEY,
+	Nombre	Varchar(30),
+	Apellido1	Varchar(30),
+	Apellido2	Varchar(30),
+	Contrasena	Varchar(max),
+	Id_Perfil	INT,
+	CONSTRAINT Fk_Usu_Per FOREIGN KEY(Id_Perfil) REFERENCES Tab_Perfiles(Id_Perfil)
+);
+
+CREATE TABLE Tab_Permisos
+(	
+	Id_Perfil	Int ,
+	Modulo	INT,
+	Agregar	VARCHAR(2),
+	Modificar	VARCHAR(2),
+	Consultar	VARCHAR(2),
+	Eliminar	VARCHAR(2),
+	PRIMARY KEY(Id_Perfil,Modulo),
+	CONSTRAINT Fk_Per_Per FOREIGN KEY(Id_Perfil) REFERENCES Tab_Perfiles(Id_Perfil)
+);
+
+CREATE TABLE Tab_Bitacora_Sesiones
+(	
+	Id_Sesion	INT PRIMARY KEY IDENTITY(1,1),
+	Fecha_inicio	DATETIME,
+	fecha_Salio	DATETIME,
+	Usuario	VARCHAR(25),
+	Direccion_Ip VARCHAR(12),
+	CONSTRAINT Fk_Bit_Usu FOREIGN KEY(Usuario) REFERENCES Tab_Usuarios(Nombre_Usuario)
+);
+
+CREATE TABLE Tab_Bitacora_Movimientos
+(	
+	Fecha_Hora	DATETIME,
+	Id_Movimiento	INT PRIMARY KEY IDENTITY(1,1),
+	Accion	VARCHAR(16),
+	Modulo	VARCHAR(25),
+	Usuario	VARCHAR(25),
+	CONSTRAINT Fk_Mov_Usu FOREIGN KEY(Usuario) REFERENCES Tab_Usuarios(Nombre_Usuario)
+);
+
+CREATE TABLE Tab_Procedimiento
+(	
+	Id_Procedimiento	INT IDENTITY(1,1) PRIMARY KEY,
+	Nombre_Procedimiento	VARCHAR(25),
+	Descripcion_Procedimiento	VARCHAR(MAX)
+);
+
+CREATE TABLE Tab_Casos
+(	
+	Cedula	INT,
+	Nombre	VARCHAR(30),
+	Apellido1	VARCHAR(30),
+	Apellido2	VARCHAR(30),
+	Genero	VARCHAR(30),
+	Num_Expediente	VARCHAR(50) PRIMARY KEY,
+	Parte_Procesal	VARCHAR(30),
+	Usuario_Generador	VARCHAR(25),
+	Lugar_Trabajo	VARCHAR(30),
+	Descripcion	VARCHAR(MAX),
+	Id_Tipo_Procedimiento	INT,
+	Medida_Cautelar	VARCHAR(MAX),
+	Organo_Director	VARCHAR(25),
+	CONSTRAINT Fk_Cas_Usu FOREIGN KEY(Usuario_Generador) REFERENCES Tab_Usuarios(Nombre_Usuario),
+	CONSTRAINT Fk_Cas_Pro FOREIGN KEY(Id_Tipo_Procedimiento) REFERENCES Tab_Procedimiento(Id_Procedimiento)
+);

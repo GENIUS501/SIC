9c7d2ce3363d802f7418a24d834559d8e4ecdc15
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2020-03-01 17:32:32 -0600

    w

--- SIC/SIC/Menu.Designer.cs
+++ SIC/SIC/Menu.Designer.cs
@@ -30,16 +30,74 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mantenimientosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seguridadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.mantenimientoDeFuncionariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mantenimientoDeCasosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mantenimientoDeRolesYPermisosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mantenimientoDeUsuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bitacoraDeIngresoYSalidaDelSistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bitacoraDeMovimientosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cambiarContraseñaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mantenimientosToolStripMenuItem,
+            this.reportesToolStripMenuItem,
+            this.seguridadToolStripMenuItem,
+            this.acercaDeToolStripMenuItem,
+            this.salirToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(686, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mantenimientosToolStripMenuItem
+            // 
+            this.mantenimientosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mantenimientoDeFuncionariosToolStripMenuItem,
+            this.mantenimientoDeCasosToolStripMenuItem});
+            this.mantenimientosToolStripMenuItem.Name = "mantenimientosToolStripMenuItem";
+            this.mantenimientosToolStripMenuItem.Size = new System.Drawing.Size(106, 20);
+            this.mantenimientosToolStripMenuItem.Text = "Mantenimientos";
+            // 
+            // reportesToolStripMenuItem
+            // 
+            this.reportesToolStripMenuItem.Name = "reportesToolStripMenuItem";
+            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.reportesToolStripMenuItem.Text = "Reportes";
+            // 
+            // seguridadToolStripMenuItem
+            // 
+            this.seguridadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mantenimientoDeRolesYPermisosToolStripMenuItem,
+            this.mantenimientoDeUsuariosToolStripMenuItem,
+            this.bitacoraDeIngresoYSalidaDelSistemaToolStripMenuItem,
+            this.bitacoraDeMovimientosToolStripMenuItem,
+            this.cambiarContraseñaToolStripMenuItem});
+            this.seguridadToolStripMenuItem.Name = "seguridadToolStripMenuItem";
+            this.seguridadToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.seguridadToolStripMenuItem.Text = "Seguridad";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.salirToolStripMenuItem.Text = "Salir";
             // 
             // statusStrip1
             // 
@@ -48,6 +106,48 @@
             this.statusStrip1.Size = new System.Drawing.Size(686, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
+            // 
+            // mantenimientoDeFuncionariosToolStripMenuItem
+            // 
+            this.mantenimientoDeFuncionariosToolStripMenuItem.Name = "mantenimientoDeFuncionariosToolStripMenuItem";
+            this.mantenimientoDeFuncionariosToolStripMenuItem.Size = new System.Drawing.Size(243, 22);
+            this.mantenimientoDeFuncionariosToolStripMenuItem.Text = "Mantenimiento de Funcionarios";
+            // 
+            // mantenimientoDeCasosToolStripMenuItem
+            // 
+            this.mantenimientoDeCasosToolStripMenuItem.Name = "mantenimientoDeCasosToolStripMenuItem";
+            this.mantenimientoDeCasosToolStripMenuItem.Size = new System.Drawing.Size(243, 22);
+            this.mantenimientoDeCasosToolStripMenuItem.Text = "Mantenimiento de casos";
+            // 
+            // mantenimientoDeRolesYPermisosToolStripMenuItem
+            // 
+            this.mantenimientoDeRolesYPermisosToolStripMenuItem.Name = "mantenimientoDeRolesYPermisosToolStripMenuItem";
+            this.mantenimientoDeRolesYPermisosToolStripMenuItem.Size = new System.Drawing.Size(260, 22);
+            this.mantenimientoDeRolesYPermisosToolStripMenuItem.Text = "Mantenimiento de roles y permisos";
+            // 
+            // mantenimientoDeUsuariosToolStripMenuItem
+            // 
+            this.mantenimientoDeUsuariosToolStripMenuItem.Name = "mantenimientoDeUsuariosToolStripMenuItem";
+            this.mantenimientoDeUsuariosToolStripMenuItem.Size = new System.Drawing.Size(260, 22);
+            this.mantenimientoDeUsuariosToolStripMenuItem.Text = "Mantenimiento de usuarios";
+            // 
+            // bitacoraDeIngresoYSalidaDelSistemaToolStripMenuItem
+            // 
+            this.bitacoraDeIngresoYSalidaDelSistemaToolStripMenuItem.Name = "bitacoraDeIngresoYSalidaDelSistemaToolStripMenuItem";
+            this.bitacoraDeIngresoYSalidaDelSistemaToolStripMenuItem.Size = new System.Drawing.Size(260, 22);
+            this.bitacoraDeIngresoYSalidaDelSistemaToolStripMenuItem.Text = "Bitacora de ingreso y salida";
+            // 
+            // bitacoraDeMovimientosToolStripMenuItem
+            // 
+            this.bitacoraDeMovimientosToolStripMenuItem.Name = "bitacoraDeMovimientosToolStripMenuItem";
+            this.bitacoraDeMovimientosToolStripMenuItem.Size = new System.Drawing.Size(260, 22);
+            this.bitacoraDeMovimientosToolStripMenuItem.Text = "Bitacora de Movimientos";
+            // 
+            // cambiarContraseñaToolStripMenuItem
+            // 
+            this.cambiarContraseñaToolStripMenuItem.Name = "cambiarContraseñaToolStripMenuItem";
+            this.cambiarContraseñaToolStripMenuItem.Size = new System.Drawing.Size(260, 22);
+            this.cambiarContraseñaToolStripMenuItem.Text = "Cambiar contraseña";
             // 
             // Menu
             // 
@@ -61,6 +161,8 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Menu";
             this.Text = "Menú";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,5 +172,17 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seguridadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientoDeFuncionariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientoDeCasosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientoDeRolesYPermisosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientoDeUsuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bitacoraDeIngresoYSalidaDelSistemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bitacoraDeMovimientosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cambiarContraseñaToolStripMenuItem;
     }
 }

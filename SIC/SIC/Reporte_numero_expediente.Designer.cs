13cb4c06a918240162cb87b1d75b8cd1d107f664
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2020-03-04 16:22:30 -0600

    0

---
+++ SIC/SIC/Reporte_numero_expediente.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace SIC
+{
+    partial class Reporte_numero_expediente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Reporte_numero_expediente));
+            this.SuspendLayout();
+            // 
+            // Reporte_numero_expediente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 298);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Reporte_numero_expediente";
+            this.Text = "Reporte por numero de expediente";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}

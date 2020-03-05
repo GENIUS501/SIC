bce6898bd52a9570177dc55b0ce852e15ace69da
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2020-03-05 12:56:04 -0600

    0

---
+++ SIC/Negocios/Gestor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocios
+{
+    public class Gestor
+    {
+        #region Mostrar especifico
+
+        #region Login
+        public Usuarios Login(string User, string Pass)
+        {
+            try
+            {
+                GestorBase BD = new GestorBase();
+                return BD.Login(User, Pass);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ SIC/Negocios/GestorBase.cs
@@ -0,0 +1,60 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocios
+{
+    public class GestorBase
+    {
+        string vCadenaConexion = Database.connectionString;
+        #region Mostrar especifico
+
+        #region Login
+        public Usuarios Login(string User, string Pass)
+        {
+            try
+            {
+                DataTable dtConsulta = new DataTable();
+                Usuarios vRegistro = new Usuarios();
+
+                string commandText = "SELECT * FROM [dbo].[Tab_Usuarios] WHERE [Nombre_Usuario] = '" + User + "' AND [Contrasena] = '" + Pass + "'";
+                //string commandText = commandTexta;
+
+                using (SqlConnection connection = new SqlConnection(vCadenaConexion))
+                {
+                    SqlCommand command = new SqlCommand(commandText, connection);
+
+                    SqlDataAdapter DataAdapter = new SqlDataAdapter(command);
+                    DataAdapter.Fill(dtConsulta);
+                }
+
+                if (dtConsulta.Rows.Count != 0)
+                {
+                    vRegistro.Cedula = int.Parse(dtConsulta.Rows[0]["Cedula"].ToString());
+                    vRegistro.Apellido1 = dtConsulta.Rows[0]["Apellido1"].ToString();
+                    vRegistro.Apellido2 = dtConsulta.Rows[0]["Apellido2"].ToString();
+                    vRegistro.Contrasena = dtConsulta.Rows[0]["Contrasena"].ToString();
+                    vRegistro.Nombre = dtConsulta.Rows[0]["Nombre"].ToString();
+                    vRegistro.Id_Perfil = int.Parse(dtConsulta.Rows[0]["Id_Perfil"].ToString());
+                    vRegistro.Nombre_Usuario = dtConsulta.Rows[0]["Nombre_Usuario"].ToString();
+                }
+
+
+                return vRegistro;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #endregion
+    }
+}

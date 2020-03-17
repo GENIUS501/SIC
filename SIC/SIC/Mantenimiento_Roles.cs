d86d557768bf44ebddf20e1328c1a92d04954cd2
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2020-03-17 12:49:43 -0600

    0

--- SIC/SIC/Mantenimiento_Roles.cs
+++ SIC/SIC/Mantenimiento_Roles.cs
@@ -27,13 +27,13 @@ namespace SIC
         {
             Negocios = new Gestor();
             Perf = new Perfiles();
+            Perf = Negocios.Mostrar_Rol(Id_Perfil);
+            this.txt_id_rol.Text = Perf.Id_Perfil.ToString();
+            this.txt_nombre.Text = Perf.Nombre_Perfil;
             Permi = new Permisos();
             #region Roles 1
             ////////Roles//1////////////////////////////////////
-            Perf = Negocios.Mostrar_Rol(Id_Perfil);
             Permi = Negocios.Mostrar_Permisos_Unico(Id_Perfil,1);
-            this.txt_id_rol.Text = Perf.Id_Perfil.ToString();
-            this.txt_nombre.Text = Perf.Nombre_Perfil;
             if (Permi.Modulo>0)
             {
                 this.clb_Roles.Enabled = true;
@@ -82,14 +82,10 @@ namespace SIC
                 this.clb_Roles.Items.Add("Consultar");
             }
             #endregion
-            Perf = new Perfiles();
             Permi = new Permisos();
             #region Usuarios 2
             ////////Usuarios//2////////////////////////////////////
-            Perf = Negocios.Mostrar_Rol(Id_Perfil);
             Permi = Negocios.Mostrar_Permisos_Unico(Id_Perfil, 2);
-            this.txt_id_rol.Text = Perf.Id_Perfil.ToString();
-            this.txt_nombre.Text = Perf.Nombre_Perfil;
             if (Permi.Modulo > 0)
             {
                 this.clb_usuarios.Enabled = true;
@@ -138,14 +134,10 @@ namespace SIC
                 this.clb_usuarios.Items.Add("Consultar");
             }
             #endregion
-            Perf = new Perfiles();
             Permi = new Permisos();
             #region Funcionarios 3
             ////////Funcionarios//3////////////////////////////////////
-            Perf = Negocios.Mostrar_Rol(Id_Perfil);
             Permi = Negocios.Mostrar_Permisos_Unico(Id_Perfil, 3);
-            this.txt_id_rol.Text = Perf.Id_Perfil.ToString();
-            this.txt_nombre.Text = Perf.Nombre_Perfil;
             if (Permi.Modulo > 0)
             {
                 this.clb_funcionarios.Enabled = true;
@@ -192,6 +184,83 @@ namespace SIC
                 this.clb_funcionarios.Items.Add("Actualizar");
                 this.clb_funcionarios.Items.Add("Borrar");
                 this.clb_funcionarios.Items.Add("Consultar");
+            }
+            #endregion
+            Permi = new Permisos();
+            #region Casos 4
+            ////////Casos//4////////////////////////////////////
+            Permi = Negocios.Mostrar_Permisos_Unico(Id_Perfil, 4);
+            if (Permi.Modulo > 0)
+            {
+                this.clb_Casos.Enabled = true;
+                this.chb_Casos.Checked = true;
+                if (Permi.Agregar == "S")
+                {
+                    this.clb_Casos.Items.Add("Agrergar", true);
+                }
+                else
+                {
+                    this.clb_Casos.Items.Add("Agrergar");
+                }
+                ///
+                if (Permi.Consultar == "S")
+                {
+                    this.clb_Casos.Items.Add("Consultar", true);
+                }
+                else
+                {
+                    this.clb_Casos.Items.Add("Consultar");
+                }
+                /////
+                if (Permi.Eliminar == "S")
+                {
+                    this.clb_Casos.Items.Add("Borrar", true);
+                }
+                else
+                {
+                    this.clb_Casos.Items.Add("Borrar");
+                }
+                /////
+                if (Permi.Modificar == "S")
+                {
+                    this.clb_Casos.Items.Add("Actualizar", true);
+                }
+                else
+                {
+                    this.clb_Casos.Items.Add("Actualizar");
+                }
+            }
+            else
+            {
+                this.clb_Casos.Items.Add("Agrergar");
+                this.clb_Casos.Items.Add("Actualizar");
+                this.clb_Casos.Items.Add("Borrar");
+                this.clb_Casos.Items.Add("Consultar");
+            }
+            #endregion
+            Permi = new Permisos();
+            #region Sesiones 5 
+            ////////Sesiones//5////////////////////////////////////
+            Permi = Negocios.Mostrar_Permisos_Unico(Id_Perfil, 5);
+            if(Permi.Modulo>0)
+            {
+                this.chb_sesiones.Checked = true;
+            }else
+            {
+
+            }
+            #endregion
+            Permi = new Permisos();
+            #region Movimientos 6 
+            ////////Movimientos//6////////////////////////////////////
+            Permi = Negocios.Mostrar_Permisos_Unico(Id_Perfil, 6);
+            if (Permi.Modulo > 0)
+            {
+                this.chb_movimientos.Checked = true;
+            }
+            else
+            {
+
             }
             #endregion
         }

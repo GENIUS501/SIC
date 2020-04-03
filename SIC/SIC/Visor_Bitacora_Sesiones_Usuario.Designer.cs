dbdfc0477e71bf6d01b20ad1578c5114f303f57a
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2020-04-03 12:03:06 -0600

    0

--- SIC/SIC/Visor_Bitacora_Sesiones_Usuario.Designer.cs
+++ SIC/SIC/Visor_Bitacora_Sesiones_Usuario.Designer.cs
@@ -28,16 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SICDataSet1 = new SIC.SICDataSet1();
+            this.Tab_Bitacora_SesionesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.Tab_Bitacora_SesionesTableAdapter = new SIC.SICDataSet1TableAdapters.Tab_Bitacora_SesionesTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.SICDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tab_Bitacora_SesionesBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // reportViewer1
             // 
             this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.Tab_Bitacora_SesionesBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "SIC.Rpt_Bitacora_Sesiones_Usuario.rdlc";
             this.reportViewer1.Location = new System.Drawing.Point(0, 0);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.Size = new System.Drawing.Size(667, 389);
             this.reportViewer1.TabIndex = 0;
+            // 
+            // SICDataSet1
+            // 
+            this.SICDataSet1.DataSetName = "SICDataSet1";
+            this.SICDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // Tab_Bitacora_SesionesBindingSource
+            // 
+            this.Tab_Bitacora_SesionesBindingSource.DataMember = "Tab_Bitacora_Sesiones";
+            this.Tab_Bitacora_SesionesBindingSource.DataSource = this.SICDataSet1;
+            // 
+            // Tab_Bitacora_SesionesTableAdapter
+            // 
+            this.Tab_Bitacora_SesionesTableAdapter.ClearBeforeFill = true;
             // 
             // Visor_Bitacora_Sesiones_Usuario
             // 
@@ -48,6 +73,8 @@
             this.Name = "Visor_Bitacora_Sesiones_Usuario";
             this.Text = "Bitacora de Sesiones";
             this.Load += new System.EventHandler(this.Visor_Bitacora_Sesiones_Usuario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SICDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tab_Bitacora_SesionesBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -55,5 +82,8 @@
         #endregion
 
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource Tab_Bitacora_SesionesBindingSource;
+        private SICDataSet1 SICDataSet1;
+        private SICDataSet1TableAdapters.Tab_Bitacora_SesionesTableAdapter Tab_Bitacora_SesionesTableAdapter;
     }
 }
--- SIC/SIC/Visor_Bitacora_Sesiones_Usuario.cs
+++ SIC/SIC/Visor_Bitacora_Sesiones_Usuario.cs
@@ -19,8 +19,17 @@ namespace SIC
 
         private void Visor_Bitacora_Sesiones_Usuario_Load(object sender, EventArgs e)
         {
+            try
+            {
+                // TODO: esta línea de código carga datos en la tabla 'SICDataSet1.Tab_Bitacora_Sesiones' Puede moverla o quitarla según sea necesario.
+                this.Tab_Bitacora_SesionesTableAdapter.Fill(this.SICDataSet1.Tab_Bitacora_Sesiones);
+                this.reportViewer1.RefreshReport();
+            }
+            catch (Exception ex)
+            {
 
-            this.reportViewer1.RefreshReport();
+            }
+
         }
     }
 }
